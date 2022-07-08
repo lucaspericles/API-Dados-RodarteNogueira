28384d9d231bd4338b611a76714d73a63cda2bde
lucaspericles <lucaspericles.fp@gmail.com>
2022-07-08 20:24:42 -0300

    iniciando projeto

---
+++ src/main/java/br/com/rodartenogueira/api/SistemaDeDadosRodarteNogueiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodartenogueira.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeDadosRodarteNogueiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeDadosRodarteNogueiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rodartenogueira/api/SistemaDeDadosRodarteNogueiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodartenogueira.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeDadosRodarteNogueiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
